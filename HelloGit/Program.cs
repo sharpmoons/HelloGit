700bb97a9272f4bbba85cb05e5a45026e91d3d78
骑士 <490476648@qq.com>
2016-09-14 04:21:50 +0800

    Dev Test

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello Git Dev Branch!");
         }
     }
 }
